a610c6e63dffd1e3e40c28717706415fa4546f56
Oliver Nikolas Winspear <dev@oliverwinspear.com>
2021-11-03 15:03:59 +1100

    Edit org-roam config

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -114,9 +114,14 @@
   :ensure t
   :init
   (setq org-roam-v2-ack t)
+  :custom
+  ;; Completion without using double square brackets.
+  (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i"   . completion-at-point))
   :config
   (org-roam-setup))
 
