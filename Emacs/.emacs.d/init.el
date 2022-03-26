521eae9a109749efa06a92973af1a394783ee343
Oliver Nikolas Winspear <dev@oliverwinspear.com>
2022-03-26 18:32:18 +1100

    Update TODO

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -11,16 +11,10 @@
 ;; 
 ;; - Add code from Protesilaos' "focused editing" setup
 ;;
+;; - Organise init.el code into different sections
+;;
 ;; Key bindings:
-;; 
-;; TREEMACS
-;; 
-;; C-x t 1    treemacs-delete-other-windows
-;; C-x t B    treemacs-bookmark
-;; C-x t C-t  treemacs-find-file
-;; C-x t M-t  treemacs-find-tag
-;; C-n        treemacs
-;; 
+;;
 ;; ORG
 ;; 
 ;; \C-cl  org-store-link
