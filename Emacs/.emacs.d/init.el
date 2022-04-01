cbd963abdb851245a6bc2ad66402eb5cc67b1170
Oliver Nikolas Winspear <dev@oliverwinspear.com>
2022-04-01 22:21:44 +1100

    Restructure Emacs.org, add no-littering
    Auto-saves are now stored in the var/ subdirectory of .emacs.d

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -10,16 +10,6 @@
 
 (set-default-coding-systems 'utf-8)
 
-(setq load-path (cons (concat user-emacs-directory "lisp") load-path))
-
-;; https://www.reddit.com/r/emacs/comments/9rrhy8/emacsers_with_beautiful_initel_files_what_about/
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-(setq backup-directory-alist `(("" . ,(concat user-emacs-directory "emacs-backup/"))))
-(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save/") t)))
-
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -37,32 +27,17 @@
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t)
 
-(use-package helm
-  :preface (require 'helm-config)
-  :config
-  ;; Open helm buffer inside current window
-  (setq helm-split-window-in-side-p t)
+(use-package no-littering)
 
-  ;; https://emacsredux.com/blog/2013/04/21/edit-files-as-root/
-  (defadvice helm-find-files (after find-file-sudo activate)
-    "Find file as root if necessary."
-    (unless (and buffer-file-name
-                 (file-writable-p buffer-file-name))
-      (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+;; Store backup and auto-save files in the var/ directory
+(setq backup-directory-alist `(("" . ,(no-littering-expand-var-file-name "emacs-backup/"))))
+(setq auto-save-file-name-transforms
+    `((".*" ,(no-littering-expand-var-file-name "emacs-auto-save/") t)))
 
-  (helm-mode 1)
-  :bind (("M-x"   . helm-M-x)
-         ("s-b"   . (lambda () (interactive) (helm-buffers-list)))
-         ("s-f"   . helm-find-files)
-         :map helm-map
-         ("<tab>" . helm-execute-persistent-action)
-         ("C-i"   . helm-execute-persistent-action)
-         ("C-z"   . helm-select-action)))
+;; Store the custom file in the etc/directory
+(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
 
-;; Requires The Silver Searcher to be installed
-(use-package helm-ag)
-
-(use-package magit)
+(push (concat user-emacs-directory "lisp/") load-path)
 
 (use-package erc
 :config
@@ -71,119 +46,6 @@
       (interactive)
       (erc-tls :server "irc.au.libera.chat"
                :port   "6697")))
-
-(define-key global-map (kbd "s-j") #'backward-char)
-(define-key global-map (kbd "s-k") #'next-line)
-(define-key global-map (kbd "s-l") #'previous-line)
-(define-key global-map (kbd "s-;") #'forward-char)
-
-(defun xah/new-empty-buffer ()
-  "Create a new empty buffer.
-New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
-
-It returns the buffer (for elisp programing).
-
-URL `http://xahlee.info/emacs/emacs/emacs_new_empty_buffer.html'
-Version 2017-11-01"
-  (interactive)
-  (let (($buf (generate-new-buffer "untitled")))
-    (switch-to-buffer $buf)
-    (funcall initial-major-mode)
-    (setq buffer-offer-save t)
-    $buf))
-
-(global-set-key (kbd "<f5>") #'xah/new-empty-buffer)
-
-(global-set-key (kbd "s-i") (lambda ()
-                              (interactive)
-                              (switch-to-buffer (find-file-noselect (concat user-emacs-directory "Emacs.org")))))
-
-(load "latexmk-mode.el")
-(add-hook 'LaTeX-mode-hook #'latexmk-mode)
-
-(use-package mixed-pitch
-  :hook (org-mode . mixed-pitch-mode)
-  :config
-  (setq mixed-pitch-set-height t))
-
-(use-package org
-  :bind (:map global-map
-              ("\C-cl" . org-store-link)
-              ("\C-ca" . org-agenda))
-
-  :config
-  (setq org-hide-emphasis-markers t)
-  (setq org-startup-indented t) ; Globally turn on Org Indent mode
-
-  (setq org-directory "/mnt/hdd/Documents")
-
-  (setq org-agenda-files '("habits.org"
-                           "projects.org"
-                           "todo.org"))
-
-  (push 'org-habit org-modules) ; Add org-habit to the list of modules
-
-  (setq org-hide-leading-stars t)
-
-  ;; Change the colour of the face that's used to hide leading stars
-  ;; The value should be #303030 for the moe-dark theme
-  (set-face-attribute 'org-hide nil :foreground "#000000")
-
-  (add-hook 'org-mode-hook #'visual-line-mode))
-
-;; Automatically tangle our Emacs.org config file when we save it
-;; From https://github.com/daviwil/emacs-from-scratch
-(defun efs/org-babel-tangle-config ()
-  (when (string-equal (file-name-directory (buffer-file-name))
-                      (expand-file-name user-emacs-directory))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
-
-(with-eval-after-load 'org
-  (org-babel-do-load-languages
-      'org-babel-load-languages
-      '((emacs-lisp . t)
-      (python . t))))
-
-(use-package visual-fill-column
-  :hook (org-mode . visual-fill-column-mode)
-  :init
-  (setq visual-fill-column-center-text t)
-  (setq visual-fill-column-width 100))
-
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode)
-  :config
-  (setq org-bullets-bullet-list '("☯" "○" "✸" "✿" "~"))
-  (setq org-bullets-face-name 'onw/org-bullets-face))
-
-(use-package org-roam
-  :init
-  (setq org-roam-v2-ack t)
-  :custom
-  (org-roam-directory "/mnt/hdd/Documents/org-roam")
-  ;; Completion without using double square brackets
-  (org-roam-completion-everywhere t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
-  :config
-  (org-roam-setup))
-
-;; This is needed as of Org 9.2
-(require 'org-tempo)
-
-(add-to-list 'org-structure-template-alist '("sh" . "src sh"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("sc" . "src scheme"))
-(add-to-list 'org-structure-template-alist '("ts" . "src typescript"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
-(add-to-list 'org-structure-template-alist '("go" . "src go"))
-(add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
-(add-to-list 'org-structure-template-alist '("json" . "src json"))
 
 (use-package pdf-tools
   :config
@@ -203,70 +65,6 @@ Version 2017-11-01"
 
   ;; Save information to a custom location
   (setq pdf-view-restore-filename (concat user-emacs-directory ".pdf-view-restore")))
-
-(setq-default tab-width 8)
-(setq-default fill-column 79)
-(setq-default indent-tabs-mode t)
-(setq-default electric-indent-inhibit t)
-
-;; Backspace tabs properly
-(setq backward-delete-char-untabify-method 'hungry)
-
-;; Highlight text between parentheses
-(setq show-paren-delay 0)
-(setq show-paren-style 'expression)
-(show-paren-mode t)
-
-(use-package aggressive-indent)
-
-(add-hook 'prog-mode-hook (lambda ()
-                            (display-fill-column-indicator-mode)
-                            (display-line-numbers-mode)))
-
-(use-package highlight-indent-guides
-  :hook (prog-mode . highlight-indent-guides-mode)
-  :config
-  (setq highlight-indent-guides-method 'character))
-
-(use-package rainbow-delimiters
-  :hook
-  ((prog-mode . rainbow-delimiters-mode)
-   (sly-mode  . rainbow-delimiters-mode))
-  :custom-face
-  (rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
-  (rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
-  (rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse")))) ; dark red
-  (rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
-  (rainbow-delimiters-depth-5-face ((t (:foreground "yellow")))) ; black
-  (rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
-  (rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
-  (rainbow-delimiters-depth-8-face ((t (:foreground "sienna1"))))
-  (whitespace-tab ((t (:foreground "#636363")))))
-
-(setq c-default-style "linux")
-
-(setq inferior-lisp-program "clisp")
-(use-package lispy)
-(use-package sly)
-(add-hook 'lisp-mode-hook (lambda ()
-                            (setq indent-tabs-mode nil)
-                            (setq fill-column 100)
-                            (aggressive-indent-mode)))
-
-(use-package anaconda-mode
-  :config
-  (add-hook 'python-mode-hook (lambda ()
-                                (setq indent-tabs-mode nil)
-                                (anaconda-mode)
-                                (anaconda-eldoc-mode))))
-
-(use-package web-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (setq web-mode-markup-indent-offset tab-width))
-
-(setq sgml-basic-offset tab-width)
-(setq css-indent-offset tab-width)
 
 (defun prot-common-crm-exclude-selected-p (input)
   "Filter out INPUT from `completing-read-multiple'.
@@ -341,8 +139,218 @@ minibuffer with something like `exit-minibuffer'."
               ("C-c C-o" . onw/play-with-mpv)
               ("s"       . prot-elfeed-search-tag-filter)))
 
+(use-package helm
+  :preface (require 'helm-config)
+  :config
+  ;; Open helm buffer inside current window
+  (setq helm-split-window-in-side-p t)
+
+  ;; https://emacsredux.com/blog/2013/04/21/edit-files-as-root/
+  (defadvice helm-find-files (after find-file-sudo activate)
+    "Find file as root if necessary."
+    (unless (and buffer-file-name
+                 (file-writable-p buffer-file-name))
+      (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+  (helm-mode 1)
+  :bind (("M-x"   . helm-M-x)
+         ("s-b"   . (lambda () (interactive) (helm-buffers-list)))
+         ("s-f"   . helm-find-files)
+         :map helm-map
+         ("<tab>" . helm-execute-persistent-action)
+         ("C-i"   . helm-execute-persistent-action)
+         ("C-z"   . helm-select-action)))
+
+;; Requires The Silver Searcher to be installed
+(use-package helm-ag)
+
+(use-package magit)
+
+(setq backward-delete-char-untabify-method 'hungry)
+
+(use-package highlight-indent-guides
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :config
+  (setq highlight-indent-guides-method 'character))
+
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+(show-paren-mode t)
+
+(use-package rainbow-delimiters
+  :hook
+  ((prog-mode . rainbow-delimiters-mode)
+   (sly-mode  . rainbow-delimiters-mode))
+  :custom-face
+  (rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
+  (rainbow-delimiters-depth-2-face ((t (:foreground "deep pink"))))
+  (rainbow-delimiters-depth-3-face ((t (:foreground "chartreuse")))) ; dark red
+  (rainbow-delimiters-depth-4-face ((t (:foreground "deep sky blue"))))
+  (rainbow-delimiters-depth-5-face ((t (:foreground "yellow")))) ; black
+  (rainbow-delimiters-depth-6-face ((t (:foreground "orchid"))))
+  (rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
+  (rainbow-delimiters-depth-8-face ((t (:foreground "sienna1"))))
+  (whitespace-tab ((t (:foreground "#636363")))))
+
+(setq-default tab-width 8)
+(setq-default fill-column 79)
+(setq-default indent-tabs-mode t)
+(setq-default electric-indent-inhibit t)
+
+(use-package aggressive-indent)
+
+(add-hook 'prog-mode-hook (lambda ()
+                            (display-fill-column-indicator-mode)
+                            (display-line-numbers-mode)))
+
+(setq c-default-style "linux")
+
+(setq inferior-lisp-program "clisp")
+(use-package lispy)
+(use-package sly)
+
+(defun onw/lisp-mode ()
+  (setq indent-tabs-mode nil)
+  (setq fill-column 100)
+  (aggressive-indent-mode))
+
+(add-hook 'lisp-mode-hook #'onw/lisp-mode)
+(add-hook 'emacs-lisp-mode-hook #'onw/lisp-mode)
+
+(use-package anaconda-mode)
+
+(add-hook 'python-mode-hook (lambda ()
+                              (setq indent-tabs-mode nil)
+                              (anaconda-mode)
+                              (anaconda-eldoc-mode)))
+
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (setq web-mode-markup-indent-offset tab-width))
+
+(setq sgml-basic-offset tab-width)
+(setq css-indent-offset tab-width)
+
+(define-key global-map (kbd "s-j") #'backward-char)
+(define-key global-map (kbd "s-k") #'next-line)
+(define-key global-map (kbd "s-l") #'previous-line)
+(define-key global-map (kbd "s-;") #'forward-char)
+
+(defun xah/new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://xahlee.info/emacs/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf))
+
+(global-set-key (kbd "<f5>") #'xah/new-empty-buffer)
+
+(global-set-key (kbd "s-i") (lambda ()
+                              (interactive)
+                              (find-file (concat user-emacs-directory "Emacs.org"))))
+
+(load "latexmk-mode.el")
+(add-hook 'LaTeX-mode-hook #'latexmk-mode)
+
+(use-package mixed-pitch
+  :hook (org-mode . mixed-pitch-mode)
+  :config
+  (setq mixed-pitch-set-height t))
+
+(use-package org
+  :bind (:map global-map
+              ("\C-cl" . org-store-link)
+              ("\C-ca" . org-agenda))
+
+  :config
+  (setq org-hide-emphasis-markers t)
+  (setq org-startup-indented t) ; Globally turn on Org Indent mode
+
+  (setq org-directory "/mnt/hdd/Documents")
+
+  (setq org-agenda-files '("habits.org"
+                           "projects.org"
+                           "todo.org"))
+
+  (push 'org-habit org-modules) ; Add org-habit to the list of modules
+
+  (setq org-hide-leading-stars t)
+
+  ;; Change the colour of the face that's used to hide leading stars
+  ;; The value should be #303030 for the moe-dark theme
+  (set-face-attribute 'org-hide nil :foreground "#000000")
+
+  (add-hook 'org-mode-hook (lambda ()
+                             (setq fill-column 100)
+                             (visual-line-mode))))
+
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t)
+      (python . t))))
+
+;; This is needed as of Org 9.2
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src sh"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("sc" . "src scheme"))
+(add-to-list 'org-structure-template-alist '("ts" . "src typescript"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+(add-to-list 'org-structure-template-alist '("go" . "src go"))
+(add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
+(add-to-list 'org-structure-template-alist '("json" . "src json"))
+
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (file-name-directory (buffer-file-name))
+                      (expand-file-name user-emacs-directory))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
+
+(use-package visual-fill-column
+  :hook (org-mode . visual-fill-column-mode)
+  :init
+  (setq visual-fill-column-center-text t)
+  (setq visual-fill-column-width 100))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :config
+  (setq org-bullets-bullet-list '("☯" "○" "✸" "✿" "~"))
+  (setq org-bullets-face-name 'onw/org-bullets-face))
+
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "/mnt/hdd/Documents/org-roam")
+  ;; Completion without using double square brackets
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
 (use-package vterm
   :bind (("s-t" . vterm-other-window)))
+
+(setq user-emacs-directory "~/stow/Emacs/.emacs.d/")
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
