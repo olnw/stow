fdb04db2016b5210009d12f0f570268f5a41fdf1
Oliver Nikolas Winspear <dev@oliverwinspear.com>
2022-05-03 17:42:51 +1000

    Don't enable flyspell-mode or flyspell-prog-mode

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -711,6 +711,3 @@ minibuffer with something like `exit-minibuffer'."
                        "#+title: ${title}\n")
     :unnarrowed t)))
   (org-roam-db-autosync-mode))
-
-(add-hook 'text-mode-hook #'flyspell-mode)
-(add-hook 'prog-mode-hook #'flyspell-prog-mode)
