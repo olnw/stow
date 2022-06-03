53f3d087bcb714de51cac268b2c46f4387c3c516
Oliver Nikolas Winspear <dev@oliverwinspear.com>
2022-06-03 18:13:04 +1000

    Disable Corfu auto-completion

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -454,23 +454,7 @@ Version 2017-11-01"
             (bound-and-true-p vertico--input))
 
   (corfu-mode 1)))
-  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1)
-  :custom
-  (corfu-auto t)
-  :hook (text-mode . (lambda () (setq-local corfu-auto nil))))
-
-  ;; TAB-and-Go customizations
-  ;; :custom
-  ;; (corfu-cycle t)             ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-preselect-first nil) ;; Disable candidate preselection
-
-  ;; Use TAB for cycling, default is 'corfu-complete'.
-  ;; :bind
-  ;; (:map corfu-map
-  ;;       ("TAB" . corfu-next)
-  ;;       ([tab] . corfu-next)
-  ;;       ("S-TAB" . corfu-previous)
-  ;;       ([backtab] . corfu-previous)))
+  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1))
 
 (use-package emacs
   :init
